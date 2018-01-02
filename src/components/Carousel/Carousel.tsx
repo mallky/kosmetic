4a534a8bc90f5d5bac6fb6907ef9c075e045e02e
mallky <makarkuz@yandex.ru>
2018-01-03 01:35:46 +0400

    (feature) - carousel done

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -3,7 +3,7 @@ import './Carousel.less';
 import * as React from 'react';
 
 import Button from '../../components/button/Button';
-import { animTypes } from '../../utils/constants';
+import { animTypes, DELAY_TIME } from '../../utils/constants';
 
 interface CarouselProps {
   images?: Array<string>;
@@ -11,65 +11,72 @@ interface CarouselProps {
 }
 
 interface CarouselState {
-  visibility: Array<any>;
   current: number;
   animType: string;
 }
 
 export default class Carousel extends React.Component<CarouselProps, CarouselState> {
+  timer: number;
+
   constructor(props: CarouselProps) {
     super(props);
 
     this.state = {
-      visibility: [],
       current: 0,
-      animType: animTypes[2],
+      animType: animTypes[0],
     };
   }
 
   componentDidMount() {
-    this.setState({
-      visibility: this.props.images.map((item, i) => !i)
-    });
-
-    const timer = setInterval(() => {
-      this.setState((prevState, props) => ({
-        current: prevState.visibility.length - 1 !== prevState.current ? prevState.current + 1 : 0,
-        visibility: this.state.visibility.map((item, i) => i === prevState.current ? prevState.current + 1 : 0),
-        animType: animTypes[2]
-      }));
-    }, 6000);
+    this._startTimer(animTypes[0]);
+  }
+  
+  componentWillUnmount() {
+    clearInterval(this.timer);
   }
 
   _setState(current, animType) {
     this.setState((prevState, props) => ({
       current: current,
-      visibility: prevState.visibility.map((item, i) => i === current),
       animType: animType
     }));
   }
 
+  _startTimer(animType) {
+    clearInterval(this.timer);
+
+    this.timer = setInterval(() => {
+      this.setState((prevState, props) => ({
+        current: this.props.images.length - 1 !== prevState.current ? prevState.current + 1 : 0,
+        animType: animType
+      }));
+    }, DELAY_TIME);
+  }
+  
   _prev() {
     const current: number = this.state.current === 0 ? this.props.images.length - 1 : this.state.current - 1;
 
     this._setState(current, animTypes[1]);
+    this._startTimer(animTypes[1]);
   }
 
   _next() {
     const current: number = this.state.current === this.props.images.length - 1 ? 0 : this.state.current + 1;
 
     this._setState(current, animTypes[0]);
+    this._startTimer(animTypes[0]);
   }
 
   _onRadioChange(e: any) {
     const currentVal: number = +e.target.value;
 
     this._setState(currentVal, animTypes[2]);
+    this._startTimer(animTypes[2]);
   }
 
   _renderImages() {
     return this.props.images.map((image, i) => {
-      return this.state.visibility[i] ? (<div key={i} className={`image-box ${this.state.animType}`} id={`image-box-${i}`}>
+      return this.state.current === i ? (<div key={i} className={`image-box ${this.state.animType}`} id={`image-box-${i}`}>
         <img src={image} alt=""/>
         {this.props.texts ? <span>{this.props.texts[i]}</span> : null}
       </div>) : null;
@@ -107,7 +114,9 @@ export default class Carousel extends React.Component<CarouselProps, CarouselSta
             icon="fa-chevron-right"
             onClick={this._next.bind(this)}/>
         </div>
-        {this.props.images ? this._renderImages() : null}
+        <div className="image-wrap-carousel">
+          {this.props.images ? this._renderImages() : null}
+        </div>
         <div className="carousel-radio-box">
           {this._renderRadioButtons(radioName)}
         </div>
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,8 +2,11 @@ const num: Array<string> = ['first', 'second', 'third', 'fouth', 'fifth'];
 const animTypes: Array<string> = ['from-left-side', 'from-right-side', 'opacity-change'];
 const tabList: Array<string> = ['главная', 'услуги и цены', 'обучение', 'магазин', 'галерея работ', 'контакты'];
 
+const DELAY_TIME = 8000;
+
 export {
   num,
   tabList,
-  animTypes
+  animTypes,
+  DELAY_TIME
 };
