8db46c92c6e605043d38c0132bf5287b084771d2
mallky <makarkuz@yandex.ru>
2018-02-17 23:13:51 +0400

    (refractor) - add configs

---
+++ dist/utils/constants.d.ts
@@ -0,0 +1,6 @@
+declare const num: Array<string>;
+declare const animTypes: Array<string>;
+declare const tabList: Array<string>;
+declare const headers: Array<string>;
+declare const DELAY_TIME = 8000;
+export { num, tabList, headers, animTypes, DELAY_TIME };
---
+++ dist/utils/functions.d.ts
@@ -0,0 +1,9 @@
+/**
+ * scroll function
+ * @param start - start position
+ * @param end - end position
+ * @param direction
+ */
+declare const scrollFunc: (start: number, end: number, direction?: number) => void;
+declare const toJSON: (form: HTMLFormElement) => string;
+export { scrollFunc, toJSON };
